247c738a3d8615a5f4cb6c29c9584c670effe86a
Zidny Ilman Nafi'an <136836161+ZidnyIlmanN@users.noreply.github.com>
2024-08-09 09:27:57 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { inject } from '@vercel/analytics';
-
-// Panggil fungsi inject untuk menambahkan skrip pelacakan
-inject();
 
 createApp(App).mount('#app');
